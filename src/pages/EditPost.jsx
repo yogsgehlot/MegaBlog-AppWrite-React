2741f21ac5bbe358b91ab3c8b5e4c096de9681f8
Yogesh Gehlot <75558691+yogsgehlot@users.noreply.github.com>
2024-03-10 23:45:51 +0530

    update

--- src/pages/EditPost.jsx
+++ src/pages/EditPost.jsx
@@ -20,7 +20,7 @@ function EditPost() {
     }
   }, [slug, navigate]);
   return post ? (
-    <div className="py-8">
+    <div className="py-8 bg-slate-900 h-screen">
       <Container>
         <PostForm post={post} />
       </Container>
